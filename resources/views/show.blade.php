53fe8f493fde409dfa6dcd4d0ec172f766a57dd8
arielkike <arielgarciae7@gmail.com>
2026-08-28 23:41:32 -0600

    fix: habilitar boton y modal de rescision total o parcial para agentes y cajeras

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -27,13 +27,11 @@
                 </button>
                 @endif
                 
-                @can('gestionar-lotificaciones')
                 @if(isset($cliente->ventas) && $cliente->ventas->first() && $cliente->ventas->first()->estado_contrato !== 'Rescindido')
                 <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#rescindirModal">
                     <i class="fas fa-ban"></i> Rescindir Venta
                 </button>
                 @endif
-                @endcan
     
                 @can('borrar-clientes')
                 <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
