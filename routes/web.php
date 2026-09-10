ea0fde77ce6e88355378612887c43915bac42da4
arielkike <arielgarciae7@gmail.com>
2026-09-09 20:33:00 -0600

    chore: remover rutas temporales tras eliminacion de bloque XYZ en produccion

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,6 @@ Route::get('/mi-estado/{token}/recibo/{abono_id}', [PortalClienteController::cla
     ->name('portal.recibo.imprimir')
     ->middleware('throttle:120,1');
 
-Route::get('/mantenimiento-bloques-campana', [ImportacionController::class, 'mantenimientoBloquesCampana']);
-
 // =========================================================================
 // RUTAS AUTENTICADAS
 // =========================================================================
